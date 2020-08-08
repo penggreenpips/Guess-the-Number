8057996a80bd80dc06a41fecdf50269fbbf587ec
penggreenpips <45311343+penggreenpips@users.noreply.github.com>
2020-08-09 02:58:17 +0800

    Update game.py
    ## Please debug in the next version, Typing STR will cause an error!

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 # This is a Guess the Number game.
+## Please do Debug, if player type string, it will cause error ##
+
 import random
 
 guessTaken = 0
@@ -6,26 +8,28 @@ guessTaken = 0
 print("Hello! What is your name?")
 myName = input()
 
-number = random.randint(1, 20)
+number = random.randint(2, 20)
+
+print('DEBUG: Number is' + ' ' + str(number)) # Debug 
+
 print('Well, ' + myName + ', I am thinkiung of a number between 1 and 20.' )
+
 
 for guessesTaken in range(6):
     print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+    guess = int(input())
 
     if guess < number:
         print('Your guess is too low.')
-    if guess > number:
+    elif guess > number:
         print('Your guess is too high')
-
-    if guess == number:
-        break
+    else:
+        break # This condition is for the correct guess!
 
 if guess == number:
     guessesTaken = str(guessesTaken + 1)
-    print('Good job,' + ' ' + myName + '! You guessed my number in' + ' ' + guessesTaken + ' guesses!')
+    print('Good job,' + ' ' + myName + '! You guessed my number in' + ' ' + str(guessesTaken) + ' guesses!')
 
 if guess != number:
     number = str(number)
-    print('Nope. The number I was thinking of was ' + number + '.')
+    print('Nope. The number I was thinking of was ' + str(number) + '.')
